b5b4c05a2b21963af666e9bafba9344a66d60a65
augusin11 <augusin11@gmail.com>
2021-01-10 18:39:42 -0800

    Updated application to use scoped_session

--- main/base.py
+++ main/base.py
@@ -1,15 +1,14 @@
 import os
 import click
 
-from flask import current_app, g
-from flask.cli import with_appcontext
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 from main.__init__ import app_instance_path
 
 engine = create_engine('sqlite:///' + os.path.join(app_instance_path, 'db.sqlite3'))
-Session = sessionmaker(bind=engine)
+session_factory = sessionmaker(bind=engine)
+Session = scoped_session(session_factory)
 
 Base = declarative_base()
 
@@ -17,11 +16,10 @@ def init_app(app):
     app.cli.add_command(init_db_command)
 
 def init_db():
-    import main.user
+    from main.user import User
     Base.metadata.create_all(engine)
 
 @click.command('init-db')
-@with_appcontext
 def init_db_command():
     init_db()
     click.echo('Initialized the database.')
