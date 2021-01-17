7d5e56437bd63e71f64bef5acf9eb158b3f927c5
augusin11 <augusin11@gmail.com>
2021-01-17 01:34:25 -0800

    Removed unused code from __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -14,11 +14,6 @@ def create_app(test_config=None):
 		DATABASE = 'sqlite:///' + os.path.join(app.instance_path, 'db.sqlite3')
 	)
 
-	if test_config is None:
-		app.config.from_pyfile('config.py', silent=True)
-	else:
-		app.config.from_mapping(test_config)
-
 	try:
 		os.makedirs(app.instance_path)
 	except OSError:
