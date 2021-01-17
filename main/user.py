645a7d78f3d1d9dc9e0d399e9ffc233e93dacc7c
augusin11 <augusin11@gmail.com>
2021-01-17 01:20:00 -0800

    Added wins and losses member variables to User class

--- main/user.py
+++ main/user.py
@@ -8,9 +8,13 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     username = Column(String)
     password = Column(String)
+    wins = Column(Integer)
+    losses = Column(Integer)
     level = Column(Integer)
 
-    def __init__(self, username, password, level=1):
+    def __init__(self, username, password, wins=0, losses=0, level=1):
         self.username = username
         self.password = password
+        self.wins = wins
+        self.losses = losses
         self.level = level
