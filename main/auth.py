26b00d6a70c115d609234fe789082b00576f3186
augusin11 <augusin11@gmail.com>
2021-01-17 18:05:47 -0800

    Added go board and game screen

--- main/auth.py
+++ main/auth.py
@@ -1,4 +1,4 @@
-from functools import wraps
+import functools
 from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for
 from werkzeug.security import check_password_hash, generate_password_hash
 
--- main/index.py
+++ main/index.py
@@ -1,17 +1,26 @@
 from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for
-
+from main.auth import login_required
 
 bp = Blueprint('index', __name__, url_prefix='/index')
 
 @bp.route('/', methods=('GET', 'POST'))
 def main_home():
     if request.method == 'POST':
-        if request.form['corner_button'] == 'sign_in':
-            return redirect(url_for('auth.login'))
-        if request.form['corner_button'] == 'log_out':
-            return redirect(url_for('auth.logout'))
+        if 'corner_button' in request.form:
+            if request.form['corner_button'] == 'sign_in':
+                return redirect(url_for('auth.login'))
+            elif request.form['corner_button'] == 'log_out':
+                return redirect(url_for('auth.logout'))
+        elif 'play_button' in request.form:
+            if request.form['play_button'] == 'start_game':
+                return redirect(url_for('index.game'))
 
     if (g.user is None):
         return render_template('index.html', is_signed_in = 0)
     else:
         return render_template('index.html', is_signed_in = 1)
+
+@bp.route('/game', methods=['GET'])
+@login_required
+def game():
+    return render_template('game.html')
