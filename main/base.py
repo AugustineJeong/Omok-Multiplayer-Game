778f7894876e5583c4cb809c42fdc7c84fcfc872
augusin11 <augusin11@gmail.com>
2021-02-03 00:12:27 -0800

    Edited license comments

--- main/base.py
+++ main/base.py
@@ -1,3 +1,12 @@
+# The following license applies to SQLAlchemy
+# Copyright (c) 2005-2021 Michael Bayer and contributors. SQLAlchemy is a trademark of Michael Bayer.
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+# Other code in file written by Augustine Jeong
+# Copyright (c) 2021 Augustine Jeong
+
 import os
 import click
 
@@ -22,4 +31,4 @@ def init_db():
 @click.command('init-db')
 def init_db_command():
     init_db()
-    click.echo('Initialized the database.')
+    click.echo('Successfully initialized the database.')
