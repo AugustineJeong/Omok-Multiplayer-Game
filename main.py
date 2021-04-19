fcc9bcc21a3e8ea9b6eefc121bc2fa4a5d7f48f0
augusin11 <augusin11@gmail.com>
2021-04-19 01:02:11 -0700

    Changed secret key to default (not the actual production key)

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ from collections import deque
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_mapping(
-	SECRET_KEY ='codingPenguins',
+	SECRET_KEY ='dev'
 )
 
 def init_app(app):
